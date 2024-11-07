e26c6cc74aa1501dc0fab75506565f9dee6ba563
Tartsi <tarvo.lilja@hotmail.fi>
2024-11-08 00:36:59 +0200

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', age: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', age: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', age: '39-23-6423122' }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
