d443b6a50881de7ba9c984c3d610b50d6c0160c2
Tartsi <tarvo.lilja@hotmail.fi>
2025-02-07 00:44:03 +0200

    3.11 puhelinluettelo full stack

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
+app.use(express.static('dist'));
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
