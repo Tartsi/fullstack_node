4b4a870a460d20a56a95867b22aa81ebd32fd065
Tartsi <tarvo.lilja@hotmail.fi>
2025-02-14 01:10:04 +0200

    3.13 puhelinluettelo ja tietokanta, step1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 app.use(express.json());
@@ -11,15 +13,10 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('dist'));
 
-let persons = [
-    { id: 1, name: 'Arto Hellas', number: '040-123456' },
-    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-];
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    Person.find({}).then(persons => {
+        res.json(persons);
+    });
 });
 
 app.get('/info', (req, res) => {
@@ -62,7 +59,7 @@ app.post('/api/persons', (req, res) => {
     res.json(newPerson);
 });
 
-const PORT =  process.env.PORT || 3001;
+const PORT =  process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
